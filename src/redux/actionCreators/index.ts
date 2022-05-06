5e9b43f989b9266c0f5634b2b3a17497ff1744d4
Henry <henrykc24@gmail.com>
2022-05-06 20:32:00 +0100

    Implement action creators

--- src/redux/actionCreators/index.ts
+++ src/redux/actionCreators/index.ts
@@ -1,27 +1,17 @@
-import { Types } from '../actionTypes';
-import { Dispatch } from 'redux';
+import { Types } from "../actionTypes";
+import { FieldSet } from "airtable";
 
-export const getStudent = (payload: string) => ({
-  type: Types.GET_STUDENT,
-  payload
+export const setEachClassStudents = (payload: FieldSet[]) => ({
+  type: Types.SET_STUDENT,
+  payload,
 });
 
-export const getClasses = (payload: string) => ({
-  type: Types.GET_CLASSES,
-  payload
+export const setCourses = (payload: string[]) => ({
+  type: Types.SET_COURSE,
+  payload,
 });
 
-export const fetchStudent = async (dispatch: Dispatch) => {
-  const url = "https://api.airtable.com/v0/app8ZbcPx7dkpOnP0/Students";
-  const result = await fetch(url, {
-    method: 'GET', // *GET, POST, PUT, DELETE, etc.
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer keysXPlo2Ngwaq5ZZ'
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-  });
-  const data = await result.json();
-  console.log(data, 'data')
-  dispatch(getStudent(data));
-};
+export const setName = (payload: string) => ({
+  type: Types.SET_NAME,
+  payload,
+});
