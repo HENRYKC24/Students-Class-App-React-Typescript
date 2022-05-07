2d4841954a6f461e31c6a9cdad062d3645421ba5
Henry <henrykc24@gmail.com>
2022-05-07 20:45:29 +0100

    Take care of cases

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import "../App.css";
 import { getAllInformation } from "../redux/getAllInformation";
+import { logout } from "../redux/actionCreators";
 
 const Login: React.FC = () => {
   const dispatch = useDispatch();
@@ -16,19 +17,32 @@ const Login: React.FC = () => {
 
   const onSubmit = (e: React.FormEvent): void => {
     e.preventDefault();
-    getAllInformation(name, dispatch, navigate);
+    const nameArray = name.trim().toLowerCase().split(' ');
+    const capitalized = nameArray.map((each) => each[0].toUpperCase() + each.slice(1));
+    const adulteratedName = capitalized.join(' ');
+    getAllInformation(adulteratedName, dispatch, navigate);
   };
+
+  useEffect(() => {
+    dispatch(logout());
+  }, [dispatch]);
 
   return (
     <div className="App">
-      <form onSubmit={(e) => onSubmit(e)}>
-        <input
-          type="text"
-          value={name}
-          className="input"
-          onChange={(e) => onChange(e)}
-        />
-        <input type="submit" className="submit" value="login"/>
+      <form onSubmit={(e) => onSubmit(e)} className="form">
+        <div className="form-controls">
+          <label className="label" htmlFor="input">
+            Student's Name
+          </label>
+          <input
+            id="input"
+            type="text"
+            value={name}
+            className="input"
+            onChange={(e) => onChange(e)}
+          />
+        </div>
+        <input type="submit" className="submit" value="login" />
       </form>
     </div>
   );
