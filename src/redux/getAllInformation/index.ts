05fc3ff1b85fbe86d1f08c93414a7dddbd62ea96
Henry <henrykc24@gmail.com>
2022-05-07 20:47:43 +0100

    Add logout implementation

--- src/redux/getAllInformation/index.ts
+++ src/redux/getAllInformation/index.ts
@@ -1,6 +1,6 @@
 import { Dispatch } from "redux";
 import Airtable, { FieldSet } from "airtable";
-import { setCourses, setEachClassStudents, setName } from "../actionCreators";
+import { setEachClassStudents, setName } from "../actionCreators";
 import { NavigateFunction } from "react-router-dom";
 
 export const getAllInformation = async (
@@ -21,12 +21,13 @@ export const getAllInformation = async (
     const records = await studentsTable.select({
       filterByFormula: `OR({Name} ='${name}')`,
     }).firstPage();
-    console.log("First Records", records)
     const fields = records.map((record) => record.fields);
-    if (!fields[0]) return;
+    if (!fields[0]) {
+      navigate('/not-found', {state:{name}});
+      return;
+    }
     const Name: any = fields[0].Name;
     const classes: any = fields[0].Classes;
-    console.log(classes, 'classes to be passed to getClasses')
     dispatch(setName(Name));
     getClasses(classes);
   }
@@ -69,30 +70,18 @@ export const getAllInformation = async (
     const records = await classesTable.select({
       filterByFormula: queryString,
     }).firstPage();
-    console.log("Second Records", records)
     const fields = records.map((record) => record.fields);
-    const classNames: any = fields.map((field) => field.Name);
+
     const studentsPerClass: any = fields.map((field) => {
-      console.log('****', field.Students, field.Name);
       return {a: field.Students, b: field.Name};
     });
-    dispatch(setCourses(classNames));
+    
     studentsPerClass.forEach(async (studentList: {a: string[], b: string}) => {
-      // const className = studentList.pop();
-      console.log(studentList, '$$$$$$$$$$$');
       const {a, b} = studentList;
-      
       let toUse = await getStudentsNamesPerClass(a, b);
-      // toUse = [...toUse];
-      console.log('toUse^^^^^^^^^^^^^^', toUse)
-
       if (toUse.length === studentsPerClass.length) {
-        // tempClassMates = toUse;
-        console.log("ToUse", toUse);
-        // const toPass = [...toUse, ]
         dispatch(setEachClassStudents(toUse));
       }
-      
     });
   }
   getOneStudent();
